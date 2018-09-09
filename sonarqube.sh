9859ade1cc27c6cb145dd4a881323208e69fd673
Matt Johnson <matt@mattpjohnson.com>
2018-09-09 18:50:14 -0400

    feat(SonarQube): Use sonar-scanner NPM package

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,16 +1,7 @@
-sonar_version=3.2.0.1227-linux
-sonar_project_dir=sonar-scanner-$sonar_version
-sonar_download_url=https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$sonar_version.zip
-
-curl $sonar_download_url -L --output sonar.zip
-unzip sonar.zip
-
-PATH=$PATH:`pwd`/$sonar_project_dir/bin
-
-sonar-scanner \
+npx sonar-scanner \
   -Dsonar.projectKey=mattpjohnson_typescript-library-boilerplate \
   -Dsonar.organization=mattpjohnson-github \
   -Dsonar.sources=. \
-  -Dsonar.exclusions=node_modules/**,coverage/**,$sonar_project_dir/** \
+  -Dsonar.exclusions=node_modules/**,coverage/** \
   -Dsonar.host.url=https://sonarcloud.io \
   -Dsonar.login=$SONAR_LOGIN
