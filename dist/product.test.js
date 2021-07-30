c8ac8f824b4fe236a6375f51132ce0794945a70f
Matt Johnson <hello@mattpjohnson.com>
2021-07-29 20:25:03 -0400

    chore: update yarn dependencies for jest

---
+++ dist/product.test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var product_1 = require("./product");
+test('multiplies 2 * 3 to equal 6', function () {
+    expect(product_1.product(2, 3)).toBe(6);
+});
+//# sourceMappingURL=product.test.js.map
---
+++ dist/sum.test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sum_1 = require("./sum");
+test('adds 1 + 2 to equal 3', function () {
+    expect(sum_1.sum(1, 2)).toBe(3);
+});
+//# sourceMappingURL=sum.test.js.map
